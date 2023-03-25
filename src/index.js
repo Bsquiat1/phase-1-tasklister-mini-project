b0957afc62e141b628d8740b00abe42a477812c5
Denis <denis.were@student.moringaschool.com>
2023-03-25 18:33:57 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const pageTitle = document.querySelector('title');
+  pageTitle.innerText = "Task Lister Lite";
 });
+const taskForm = document.querySelector('#create-task-form');
+const taskInput = document.querySelector('#new-task-description');
+
+taskForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const taskList = document.querySelector('#tasks');
+  const taskItem = document.createElement('li');
+  taskItem.innerText = taskInput.value;
+  taskList.appendChild(taskItem);
+  taskInput.value = '';
+
+})
